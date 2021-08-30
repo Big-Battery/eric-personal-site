0155d07488c10274bbbb34c9fff29ee375a1b831
Dylan Cole <dyudis314@gmail.com>
2021-08-30 10:43:55 -0700

    update icon and copy

--- assets/js/calendly.js
+++
@@ -1,8 +0,0 @@
-Calendly.initInlineWidget({
-    url: 'https://calendly.com/YOURLINK',
-    parentElement: document.getElementById('calendly'),
-    prefill: {},
-    utm: {}
-   });
-
-   console.log('hi')
